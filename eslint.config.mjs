030ad8ac3c8fc61c32a62ec138bf029d5105d191
Štefan Šimek <simek@triaxis.sk>
2025-01-21 23:20:51 +0100

    style: add jsdoc and perfectionist sort-import rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
+import jsdoc from 'eslint-plugin-jsdoc'
+import perfectionist from 'eslint-plugin-perfectionist'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  jsdoc.configs['flat/contents-typescript-error'],
+  jsdoc.configs['flat/logical-typescript-error'],
+  jsdoc.configs['flat/stylistic-typescript-error'],
   stylistic.configs.customize({
     quotes: 'single',
+    braceStyle: '1tbs',
   }),
   {
     languageOptions: {
@@ -20,6 +26,13 @@ export default tseslint.config(
       },
       ecmaVersion: 2022,
       sourceType: 'module',
+    },
+
+    plugins: { perfectionist },
+
+    rules: {
+      'perfectionist/sort-exports': 'error',
+      'perfectionist/sort-imports': 'error',
     },
   },
   {
