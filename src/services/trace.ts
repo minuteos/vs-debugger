2535b654f9c004c1e8661d12b3e97cfeb2e810f7
Štefan Šimek <simek@triaxis.sk>
2025-01-27 12:04:10 +0100

    feat(trace): add wildcard suppot to trace categories

--- src/services/trace.ts
+++ src/services/trace.ts
@@ -1,15 +1,43 @@
 import { getLog } from '@my/services'
-import { settings } from '@my/settings'
+import { Settings, settings } from '@my/settings'
+
+let categories: Record<string, boolean> | undefined
+
+let cache: {
+  settings: Settings
+  categories?: Record<string, boolean>
+  enabled: Record<string, boolean>
+} | undefined
+
+function getCache(): Record<string, boolean> {
+  if (cache && cache.settings === settings && cache.categories === categories) {
+    return cache.enabled
+  }
+
+  const escape = (s: string) => s.replaceAll(/[.+*|]/g, '\\$&')
+  const match = new RegExp('^(' + settings.trace.map(escape).join('|').replace('\\*', '.*') + ')$', 'i')
+
+  return (cache = {
+    settings,
+    categories,
+    enabled: categories
+      ? Object.fromEntries(Object.keys(categories).map(k => [k, !!match.exec(k)]))
+      : {},
+  }).enabled
+}
 
 export function traceEnabled(category: string): boolean {
-  return !!settings.trace[category.toLowerCase()]
+  return getCache()[category]
 }
 
 export function getTrace(category: string): (...args: unknown[]) => void {
+  if (!categories?.[category]) {
+    categories = { ...categories, [category]: true }
+  }
+
   const log = getLog(category)
-  const traceProp = category.toLowerCase()
   return (...args) => {
-    if (settings.trace[traceProp]) {
+    if (traceEnabled(category)) {
       log.trace(...args)
     }
   }
--- src/settings.ts
+++ src/settings.ts
@@ -1,37 +1,31 @@
-import { getLog } from '@my/services'
+import { getLog, getTrace } from '@my/services'
 import * as vscode from 'vscode'
 
 const log = getLog('Settings')
+const trace = getTrace('Settings')
 
 export interface Settings {
-  trace: Record<string, boolean>
-}
-
-const defaults: Settings = {
-  trace: {},
-}
-
-export const settings = vsLoadSettings()
-
-interface VsSettings extends Omit<Settings, 'trace'> {
   trace: string[]
 }
 
-function vsLoadSettings(): Settings {
-  const settings: Settings = { ...defaults }
+const defaults: Settings = Object.freeze({
+  trace: [],
+})
 
+export let settings = defaults
+
+vsLoadSettings()
+
+type VsSettings = Settings
+
+function vsLoadSettings(): Settings {
   function loadConfig() {
     // eslint-disable-next-line @typescript-eslint/unbound-method, @typescript-eslint/no-unused-vars
     const { has, get, update, inspect, ...vsCfg } = vscode.workspace.getConfiguration('minuteDebug')
     const vs: VsSettings = vsCfg as VsSettings
-    const patches = {
-      trace: Object.fromEntries(vs.trace.map(k => [k.toLowerCase(), true])),
-    }
-    Object.assign(settings, { ...defaults, ...vs, ...patches })
-    // cannot use traceEnabled at this point, because the settings global may not be initialized
-    if (settings.trace.settings) {
-      log.trace('Current Settings', settings)
-    }
+    const patches = {}
+    settings = Object.freeze({ ...defaults, ...vs, ...patches })
+    trace(settings)
   }
 
   vscode.workspace.onDidChangeConfiguration(() => {
