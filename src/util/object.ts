02c520ce650b87c6e34dba58e080278a45a2cca0
Štefan Šimek <simek@triaxis.sk>
2025-01-31 08:28:58 +0100

    feat(util): add pick/omit helpers

--- src/util/object.ts
+++ src/util/object.ts
@@ -3,3 +3,29 @@ export function getProperty(target: unknown, property: PropertyKey): unknown {
     ? (target as Record<PropertyKey, unknown>)[property]
     : undefined
 }
+
+export function pick<T extends object, K extends keyof T>(obj: T, ...keys: K[]) {
+  return Object.fromEntries(
+    keys
+      .filter(key => key in obj)
+      .map(key => [key, obj[key]]),
+  ) as Pick<T, K>
+}
+
+export function inclusivePick<T extends object, K extends PropertyKey>(
+  obj: T, ...keys: K[]
+) {
+  return Object.fromEntries(
+    keys
+      .map(key => [key, obj[key as unknown as keyof T]]),
+  ) as { [key in K]: key extends keyof T ? T[key] : undefined }
+}
+
+export function omit<T extends object, K extends keyof T>(
+  obj: T, ...keys: K[]
+) {
+  return Object.fromEntries(
+    Object.entries(obj)
+      .filter(([key]) => !keys.includes(key as K)),
+  ) as Omit<T, K>
+}
